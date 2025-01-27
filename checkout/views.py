62ff2b074664dd032239ff040fdc106387d31a1a
D <dylankfilm@gmail.com>
2025-01-27 14:30:41 +0900

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QlkhOFg17uaRuKaIbwdKt4q1eDIujQSFuAeLzWioxwdSlPjr2V68XGOTtpkQJHAd2JPOxC6eCuLplBaQ21BJuVF00ONFCD351',
+        'client_secret': 'test client secret',
     }
     
     return render(request, template, context)
