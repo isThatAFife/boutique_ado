ba41d9013b42f544a12654f55f21391accf5b818
D <dylankfilm@gmail.com>
2025-01-28 16:42:51 +0900

    completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -5,22 +5,25 @@ from .models import Order, OrderLineItem
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
-    
+
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
-    
+
     readonly_fields = ('order_number', 'date',
-                        'delivery_cost', 'order_total', 'grand_total')
-    
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_bag', 'stripe_pid')
+
     fields = ('order_number', 'date', 'full_name',
-                'email', 'phone_number', 'country', 'postcode',
-                'town_or_city', 'street_address1', 'street_address2',
-                'county', 'delivery_cost', 'order_total', 'grand_total')
-    
+              'email', 'phone_number', 'country', 'postcode',
+              'town_or_city', 'street_address1', 'street_address2',
+              'county', 'delivery_cost', 'order_total', 'grand_total'
+              'original_bag', 'stripe_pid')
+
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost', 'grand_total')
-    
-    ordering = ('-date',) # newest orders at the top
-    
+
+    ordering = ('-date',)  # newest orders at the top
+
+
 admin.site.register(Order, OrderAdmin)
