14f2104f5edcb397aac1dbc91bb253aaa86dc46e
D <dylankfilm@gmail.com>
2025-02-07 11:43:06 +0900

    added profile form, views & updated template

--- profiles/models.py
+++ profiles/models.py
@@ -9,12 +9,12 @@ from django_countries.fields import CountryField
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, blank=True, null=True)
-    default_country = CountryField(max_length=40, blank=True, null=True)
-    default_postcode = models.CharField(max_length=20, blank=True, null=True)
-    default_town_or_city = models.CharField(max_length=40, blank=True, null=True)
     default_street_address1 = models.CharField(max_length=80, blank=True, null=True)
     default_street_address2 = models.CharField(max_length=80, blank=True, null=True)
+    default_town_or_city = models.CharField(max_length=40, blank=True, null=True)
     default_county = models.CharField(max_length=80, blank=True, null=True)
+    default_postcode = models.CharField(max_length=20, blank=True, null=True)
+    default_country = CountryField(blank_label='Country', max_length=40, blank=True, null=True)
     
     def __str__(self):
         return self.user.username
